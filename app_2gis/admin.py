b81fdd2d5e36957bc33b8f4b1bd824af483b272c
rodakalex <ravengothic@mail.ru>
2021-01-18 18:52:07 +0300

    Создал базу координат

--- app_2gis/admin.py
+++ app_2gis/admin.py
@@ -4,7 +4,19 @@ from .models import *
 
 @admin.register(Path)
 class PointerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Coord)
+class CoordAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(TypeCoord)
+class TypeCoordAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.register(Coord, CoordAdmin)
 admin.register(Path, PointerAdmin)
+admin.register(TypeCoord, TypeCoordAdmin)
--- app_2gis/models.py
+++ app_2gis/models.py
@@ -1,20 +1,44 @@
 from django.db import models
 
 
+class TypeCoord(models.Model):
+    """Тип маршрута не задаётся пользователем, только через админку"""
+    title = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, null=False, verbose_name="URL")
+
+    class Meta:
+        verbose_name = "Тип координат"
+        verbose_name_plural = "Тип координат"
+
+    def __str__(self):
+        return self.title
+
+
 class Coord(models.Model):
-    name = models.CharField(max_length=255, verbose_name="")
+    """Несколько координат может использоваться для нескольких маршрутов"""
+    title = models.CharField(max_length=255, verbose_name="Название")
     latitude = models.DecimalField(decimal_places=6, verbose_name="Широта", max_digits=8)
     longitude = models.DecimalField(decimal_places=6, verbose_name="Долгота", max_digits=8)
 
+    class Meta:
+        verbose_name = "Координаты"
+        verbose_name_plural = "Координаты"
+
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class Path(models.Model):
     title = models.CharField(max_length=255, verbose_name="Маршрут")
-    slug = models.SlugField(unique=True, verbose_name="url")
+    slug = models.SlugField(unique=True, verbose_name="url", null=False)
     describe = models.TextField(verbose_name="Описание")
-    coord = models.ManyToManyField(Coord)
+    coord = models.ManyToManyField(Coord, verbose_name="Точки")
+    type = models.ManyToManyField(TypeCoord, verbose_name="Тип маршрута")
+    public = models.BooleanField(null=False, default=True, verbose_name="Публичный")
+
+    class Meta:
+        verbose_name = "Маршрут"
+        verbose_name_plural = "Маршруты"
 
     def __str__(self):
         return self.title
